180bf26a7df287b3184e598b7755f7283a710e87
Meghan La Breche <Megavictoria@gmail.com>
2021-12-12 19:52:33 -0800

    text & Password

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -29,6 +29,14 @@ const HomeScreen = ({ navigation }) => {
 				title='Go to Square Screen'
 				onPress={() => navigation.navigate('Square')}
 			/>
+			<Button
+				title='Go to Text Screen'
+				onPress={() => navigation.navigate('Text')}
+			/>
+			<Button
+				title='Go to Box Screen'
+				onPress={() => navigation.navigate('Box')}
+			/>
 		</View>
 	);
 };
